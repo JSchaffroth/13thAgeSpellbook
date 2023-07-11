131c87de859d47f5916189b4bcab36f15aaac9ce
JSchaffroth <jonathanschaffroth@yahoo.com>
2023-07-10 22:53:41 -0700

    Implemented Label Saving
    -Can now save the label!
    -Only works for first checkbox in barbarian
    -When unchecked, label element is removed and will not display on the compendium page

--- Jonathan_s 13th Age Spellbook/pages/checkboxes.js
+++ Jonathan_s 13th Age Spellbook/pages/checkboxes.js
@@ -12,22 +12,37 @@ function saveToCompendium(obj)
 }
 
 // Retrieve checkbox elements
-const barbarian_0 = document.getElementById('barbarian_barbarianRage');
+const barbarian_box_0 = document.getElementById('barbarian_barbarianRage');
+
+// Retrieve label elements
+const barbarian_label_0 = document.querySelector('label[for="barbarian_barbarianRage"]');
 	
 // Saves checkbox state to local storage
 function saveCheckboxStates()
 {
-	localStorage.setItem('barbarian_barbarianRage', barbarian_0.checked);
+	localStorage.setItem('barbarian_boxCheck_0', barbarian_box_0.checked);
+	
+	if (!barbarian_box_0.checked)
+		localStorage.removeItem('barbarian_labelText_0');
 }
+
+// Saves label text to local storage
+function saveLabelText()
+{
+	localStorage.setItem('barbarian_labelText_0', barbarian_label_0.textContent);
+}
+
+// Save label text
+saveLabelText();
 	
 // Loads checkbox states from local storage
 function loadCheckboxStates()
 {
-	barbarian_0.checked = localStorage.getItem('barbarian_barbarianRage') === 'true';
+	barbarian_box_0.checked = localStorage.getItem('barbarian_boxCheck_0') === 'true';
 }
 	
-// Attache event listeners to checkboxes to save their states
-barbarian_0.addEventListener('change', saveCheckboxStates);
+// Attach event listeners to checkboxes to save their states
+barbarian_box_0.addEventListener('change', saveCheckboxStates);
 	
 // Load checkbox states when the page loads
 window.addEventListener('load', loadCheckboxStates);
