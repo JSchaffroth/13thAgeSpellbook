1ab823fe43545226d61e0220be7f63173662a04c
JSchaffroth <jonathanschaffroth@yahoo.com>
2023-07-10 21:52:57 -0700

    Saving Checkbox State Pt1
    -Does not work yet on my end, having Tyler try

--- Jonathan_s 13th Age Spellbook/pages/checkboxes.js
+++ Jonathan_s 13th Age Spellbook/pages/checkboxes.js
@@ -1,7 +1,33 @@
+////////////////////////////////////////////////////////////////////////
+//                         Checkbox Code                              //
+////////////////////////////////////////////////////////////////////////
+
 function saveToCompendium(obj)
 {
 	if (obj.checked)
 	{
 		alert("Yes checked " + obj.id);
 	}
+	
 }
+
+// Retrieve checkbox elements
+const barbarian_0 = document.getElementById('barbarian_barbarianRage');
+	
+// Saves checkbox state to local storage
+function saveCheckboxStates()
+{
+	localStorage.setItem('barbarian_barbarianRage', barbarian_0.checked);
+}
+	
+// Loads checkbox states from local storage
+function loadCheckboxStates()
+{
+	barbarian_0.checked = localStorage.getItem('barbarian_barbarianRage') === 'true';
+}
+	
+// Attache event listeners to checkboxes to save their states
+barbarian_0.addEventListener('change', saveCheckboxStates);
+	
+// Load checkbox states when the page loads
+window.addEventListener('load', loadCheckboxStates);
